1fb2dc8e5d66f2b8eebfa1a1dd1b727fd7cdb9e1
skiera <skiera@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2009-06-27 06:54:07 +0000

    + Initial Import

---
+++ trunk/plugins/org.phpsrc.eclipse.pti.tool.phpunit/src/org/phpsrc/eclipse/pti/tool/phpunit/PHPUnitPlugin.java
@@ -0,0 +1,64 @@
+package org.phpsrc.eclipse.pti.tool.phpunit;
+
+import org.eclipse.core.runtime.IPath;
+import org.osgi.framework.BundleContext;
+import org.phpsrc.eclipse.pti.core.AbstractPHPToolPlugin;
+import org.phpsrc.eclipse.pti.library.pear.PHPLibraryPEARPlugin;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class PHPUnitPlugin extends AbstractPHPToolPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.phpsrc.eclipse.pti.tool.phpunit";
+
+	// The shared instance
+	private static PHPUnitPlugin plugin;
+
+	/**
+	 * The constructor
+	 */
+	public PHPUnitPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static PHPUnitPlugin getDefault() {
+		return plugin;
+	}
+
+	@Override
+	public IPath[] getPluginIncludePaths() {
+		return new IPath[] { PHPLibraryPEARPlugin.getDefault().resolvePluginResource("/php/library"),
+				PHPLibraryPEARPlugin.getDefault().resolvePluginResource("/php/library/PEAR"),
+				resolvePluginResource("/php/tools") };
+	}
+}
