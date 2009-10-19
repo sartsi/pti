a5a3edcbc201c9dd945e3f4fbe194c6128f8fbbe
spasic-dejan <spasic-dejan@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2009-10-19 21:24:38 +0000

    Basic Framework

---
+++ trunk/plugins/org.phpsrc.eclipse.pti.phpbeautifier/src/plugin_org/phpsrc/eclipse/pti/phpbeautifier/Activator.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright (c) 2009, Dejan Spasic
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - Neither the name of the Organisation nor the names of its contributors may
+ *   be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+package plugin_org.phpsrc.eclipse.pti.phpbeautifier;
+
+import org.eclipse.core.runtime.IPath;
+import org.osgi.framework.BundleContext;
+import org.phpsrc.eclipse.pti.core.AbstractPHPToolPlugin;
+import org.eclipse.core.resources.IProject;
+import org.phpsrc.eclipse.pti.library.pear.PHPLibraryPEARPlugin;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractPHPToolPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "Plugin_org.phpsrc.eclipse.pti.phpbeautifier";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Provides the include paths
+	 * 
+	 * @param IProject project The current project resource
+	 * 
+	 * @return IPath[]
+	 * 
+	 * (non-Javadoc)
+	 * @see org.phpsrc.eclipse.pti.core.AbstractPHPToolPlugin#getPluginIncludePaths(org.eclipse.core.resources.IProject)
+	 */
+	@Override
+	public IPath[] getPluginIncludePaths(IProject project) {
+		IPath[] pearPaths = PHPLibraryPEARPlugin.getDefault().getPluginIncludePaths(project);
+
+		IPath[] includePaths = new IPath[pearPaths.length + 1];
+		includePaths[0] = resolvePluginResource("/php/bin");
+		System.arraycopy(pearPaths, 0, includePaths, 1, pearPaths.length);
+
+		return includePaths;
+	}
+}
