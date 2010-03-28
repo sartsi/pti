ccb7e4eca7a010329f0fd4d139792410ce4a5607
skiera <skiera@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2010-03-28 19:14:35 +0000

    Ticket 33: add missing metrics

--- plugins/org.phpsrc.eclipse.pti.tool.phpdepend/trunk/src/org/phpsrc/eclipse/pti/tools/phpdepend/core/preferences/Metric.java
+++ plugins/org.phpsrc.eclipse.pti.tool.phpdepend/trunk/src/org/phpsrc/eclipse/pti/tools/phpdepend/core/preferences/Metric.java
@@ -32,6 +32,16 @@ public class Metric {
 	public static final int TYPE_FILE_WITH_HIERACHY = 2;
 	public static final int TYPE_PACKAGE = 3;
 
+	public static final int PROPERTY_PROJECT = 1;
+	public static final int PROPERTY_PACKAGE = 2;
+	public static final int PROPERTY_CLASS = 4;
+	public static final int PROPERTY_INTERFACE = 8;
+	public static final int PROPERTY_METHOD = 16;
+	public static final int PROPERTY_FUNCTION = 32;
+
+	public static final int PROPERTY_ALL = Metric.PROPERTY_PROJECT | Metric.PROPERTY_PACKAGE | Metric.PROPERTY_CLASS
+			| Metric.PROPERTY_INTERFACE | Metric.PROPERTY_METHOD | Metric.PROPERTY_FUNCTION;
+
 	public boolean enabled;
 	public String id;
 	public String name;
@@ -40,14 +50,21 @@ public class Metric {
 	public Float errorMax;
 	public Float errorMin;
 	public int type;
+	public int properties;
 
 	public Metric() {
+		this(null, null);
 	}
 
 	public Metric(String id, String name) {
+		this(id, name, 0);
+	}
+
+	public Metric(String id, String name, int properties) {
 		this.enabled = false;
 		this.id = id;
 		this.name = name;
+		this.properties = properties;
 	}
 
 	public String toString() {
--- plugins/org.phpsrc.eclipse.pti.tool.phpdepend/trunk/src/org/phpsrc/eclipse/pti/tools/phpdepend/core/preferences/MetricList.java
+++ plugins/org.phpsrc.eclipse.pti.tool.phpdepend/trunk/src/org/phpsrc/eclipse/pti/tools/phpdepend/core/preferences/MetricList.java
@@ -2,18 +2,64 @@ package org.phpsrc.eclipse.pti.tools.phpdepend.core.preferences;
 
 public class MetricList {
 	public static Metric[] getAll() {
-		return new Metric[] { new Metric("cis", "Class interface size"), new Metric("cloc", "Comment lines of code"),
-				new Metric("cr", "Code rank"), new Metric("csz", "Class size"),
+		return new Metric[] {
+				// ClassLevel (Class)
+				new Metric("dit", "Depth of Inheritance Tree", Metric.PROPERTY_CLASS),
+				new Metric("impl", "Numer of impemented interfaces", Metric.PROPERTY_CLASS),
+				new Metric("cis", "Class interface size", Metric.PROPERTY_CLASS),
+				new Metric("csz", "Class size", Metric.PROPERTY_CLASS),
+				new Metric("vars", "Number of defined class properties", Metric.PROPERTY_CLASS),
+				new Metric("varsi", "Number of own and inherited class properties", Metric.PROPERTY_CLASS),
+				new Metric("varsnp", "Number of public class properties", Metric.PROPERTY_CLASS),
+				new Metric("wmc", "Weighted Method per Class", Metric.PROPERTY_CLASS),
+				new Metric("wmci", "Weighted Method per Class + inherited WMC", Metric.PROPERTY_CLASS),
+				new Metric("wmcnp", "Wighted Method per Class for all public class methods", Metric.PROPERTY_CLASS),
+				// CodeRank (Class, Package)
+				new Metric("cr", "Code rank", Metric.PROPERTY_CLASS | Metric.PROPERTY_PACKAGE),
+				new Metric("rcr", "Reverse code rank", Metric.PROPERTY_CLASS | Metric.PROPERTY_PACKAGE),
+				// Coupling (Project)
+				new Metric("calls", "Number of called methods and functions", Metric.PROPERTY_PROJECT),
+				new Metric("fanout", "Number of referenced types", Metric.PROPERTY_PROJECT),
+				// Cyclomatic Complexity (Project, Method, Function)
+				new Metric("ccn", "Cyclomatic Complexity", Metric.PROPERTY_PROJECT | Metric.PROPERTY_METHOD
+						| Metric.PROPERTY_FUNCTION),
+				new Metric("ccn2", "Extended Cyclomatic Complexity", Metric.PROPERTY_PROJECT | Metric.PROPERTY_METHOD
+						| Metric.PROPERTY_FUNCTION),
+				// Dependency (Package)
+				new Metric("ca", "Afferent coupling", Metric.PROPERTY_PACKAGE),
+				new Metric("ce", "Efferent coupling", Metric.PROPERTY_PACKAGE),
+				new Metric("a", "Abstraction", Metric.PROPERTY_PACKAGE),
+				new Metric("i", "Instability", Metric.PROPERTY_PACKAGE),
+				new Metric("d", "Distance", Metric.PROPERTY_PACKAGE),
+				// Hierachy (Project)
+				new Metric("clsa", "Number of abstract classes", Metric.PROPERTY_PROJECT),
+				new Metric("clsc", "Number of concrete classes", Metric.PROPERTY_PROJECT),
+				new Metric("roots", "Number of root classes within the analyzed system", Metric.PROPERTY_PROJECT),
+				new Metric("leafs", "Number of leaf classes", Metric.PROPERTY_PROJECT),
+				new Metric("maxDIT", "Max Depth of Inheritance Tree value", Metric.PROPERTY_PROJECT),
+				// Inheritance (Project)
+				new Metric("andc", "Average Number of Derived Classes", Metric.PROPERTY_PROJECT),
+				new Metric("ahh", "Average Hierachy Height", Metric.PROPERTY_PROJECT),
+				// NodeCount (Project, Package, Class, Interface)
+				new Metric("nop", "Number Of Packages", Metric.PROPERTY_PROJECT | Metric.PROPERTY_PACKAGE
+						| Metric.PROPERTY_CLASS | Metric.PROPERTY_INTERFACE),
+				new Metric("noc", "Number Of Classes", Metric.PROPERTY_PROJECT | Metric.PROPERTY_PACKAGE
+						| Metric.PROPERTY_CLASS | Metric.PROPERTY_INTERFACE),
+				new Metric("noi", "Number Of Interfaces", Metric.PROPERTY_PROJECT | Metric.PROPERTY_PACKAGE
+						| Metric.PROPERTY_CLASS | Metric.PROPERTY_INTERFACE),
+				new Metric("nom", "Number Of Methods", Metric.PROPERTY_PROJECT | Metric.PROPERTY_PACKAGE
+						| Metric.PROPERTY_CLASS | Metric.PROPERTY_INTERFACE),
+				new Metric("non", "Number Of Functions", Metric.PROPERTY_PROJECT | Metric.PROPERTY_PACKAGE
+						| Metric.PROPERTY_CLASS | Metric.PROPERTY_INTERFACE),
+				// NodeLOC (Project, Package, Class, Interface, Method,
+				// Function)
+				new Metric("loc", "Lines of code", Metric.PROPERTY_ALL),
+				new Metric("cloc", "Comment lines of code", Metric.PROPERTY_ALL),
+				new Metric("ncloc", "Non-comment lines of code", Metric.PROPERTY_ALL),
+				// Other
 				new Metric("dit", "Depth of inheritence tree"), new Metric("eloc", "Executable lines of code"),
-				new Metric("impl", "Numer of impemented interfaces"), new Metric("loc", "Lines of code"),
-				new Metric("ncloc", "Non-comment lines of code"), new Metric("nom", "Number of methods"),
-				new Metric("rcr", "Reverse code rank"), new Metric("vars", "Number of defined class properties"),
-				new Metric("varsi", "Number of own and inherited class properties"),
-				new Metric("varsnp", "Number of public class properties"),
-				new Metric("wmc", "Weighted Method per Class"),
-				new Metric("wmci", "Weighted Method per Class + inherited WMC"),
-				new Metric("wmcnp", "Wighted Method per Class for all public class methods"),
-				new Metric("ccn", "Cyclomatic Complexity"), new Metric("ccn2", "Extended Cyclomatic Complexity"),
-				new Metric("npath", "NPath Complexity"), };
+				new Metric("nom", "Number of methods"), new Metric("npath", "NPath Complexity"),
+				new Metric("noam", "Number of added methods"), new Metric("nocc", "Number of child classes"),
+				new Metric("noom", "Number of overwritten methods"), };
 	}
 }
