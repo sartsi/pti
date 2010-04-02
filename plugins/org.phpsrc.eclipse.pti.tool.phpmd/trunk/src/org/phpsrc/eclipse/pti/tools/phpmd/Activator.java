86a0a8f36a788f1e98b9053ff37fe0f1a1bbeadc
spasic.dejan <spasic.dejan@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2010-04-02 12:05:10 +0000

    etends now AbstractPHPToolPlugin  and implement the getPluginIncludePaths

--- plugins/org.phpsrc.eclipse.pti.tool.phpmd/trunk/src/org/phpsrc/eclipse/pti/tools/phpmd/Activator.java
+++ plugins/org.phpsrc.eclipse.pti.tool.phpmd/trunk/src/org/phpsrc/eclipse/pti/tools/phpmd/Activator.java
@@ -1,13 +1,16 @@
 package org.phpsrc.eclipse.pti.tools.phpmd;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.osgi.framework.BundleContext;
+import org.phpsrc.eclipse.pti.core.AbstractPHPToolPlugin;
+import org.phpsrc.eclipse.pti.library.pear.PHPLibraryPEARPlugin;
 import org.phpsrc.eclipse.pti.tools.phpmd.core.Phpmd;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractPHPToolPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "Plugin_org.phpsrc.eclipse.pti.tools.phpmd";
 
@@ -61,5 +64,21 @@ public class Activator extends AbstractUIPlugin {
 		}
 
 		return phpmd;
+	}
+
+	/**
+	 * Provides all include paths for the plugin
+	 * 
+	 * @return the include paths
+	 */
+	public IPath[] getPluginIncludePaths(IProject project) {
+		IPath[] pearPaths = PHPLibraryPEARPlugin.getDefault()
+				.getPluginIncludePaths(project);
+
+		IPath[] includePaths = new IPath[pearPaths.length + 1];
+		includePaths[0] = resolvePluginResource("/php/tools");
+		System.arraycopy(pearPaths, 0, includePaths, 1, pearPaths.length);
+
+		return includePaths;
 	}
 }
