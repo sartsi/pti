94aee794ebc2ef1d9781f6161bfb009b7db6bb3e
spasic.dejan <spasic.dejan@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2010-04-02 11:42:53 +0000

    implement the main phpmd class

---
+++ plugins/org.phpsrc.eclipse.pti.tool.phpmd/trunk/src/org/phpsrc/eclipse/pti/tools/phpmd/core/Phpmd.java
@@ -0,0 +1,9 @@
+package org.phpsrc.eclipse.pti.tools.phpmd.core;
+
+import org.eclipse.core.runtime.QualifiedName;
+import org.phpsrc.eclipse.pti.core.tools.AbstractPHPTool;
+import org.phpsrc.eclipse.pti.tools.phpmd.Activator;
+
+public class Phpmd extends AbstractPHPTool {
+	public final static QualifiedName QUALIFIED_NAME = new QualifiedName(Activator.PLUGIN_ID, "phpmd");
+}
