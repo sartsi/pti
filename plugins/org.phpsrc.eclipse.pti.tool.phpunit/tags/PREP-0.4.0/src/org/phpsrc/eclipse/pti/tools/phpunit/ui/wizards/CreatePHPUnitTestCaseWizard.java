d711923d14283e9222b4f73e6a2e8b15916f55a5
skiera <skiera@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2010-04-12 08:53:57 +0000

    0.4.0 Development Version

---
+++ plugins/org.phpsrc.eclipse.pti.tool.phpunit/tags/PREP-0.4.0/src/org/phpsrc/eclipse/pti/tools/phpunit/ui/wizards/CreatePHPUnitTestCaseWizard.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 Sven Kiera
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.phpsrc.eclipse.pti.tools.phpunit.ui.wizards;
+
+import java.io.InvalidClassException;
+import java.io.InvalidObjectException;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.dltk.core.search.IDLTKSearchScope;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
+import org.phpsrc.eclipse.pti.tools.phpunit.core.PHPUnit;
+import org.phpsrc.eclipse.pti.ui.Logger;
+
+public class CreatePHPUnitTestCaseWizard extends Wizard implements INewWizard {
+
+	private IWorkbench workbench;
+	private IStructuredSelection selection;
+	private PHPUnitTestCaseCreationWizardPage sourceClassPage;
+
+	public CreatePHPUnitTestCaseWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+
+	public boolean performFinish() {
+		if (sourceClassPage.finish()) {
+			PHPUnit phpunit = PHPUnit.getInstance();
+			try {
+				boolean ok = phpunit.createTestSkeleton(sourceClassPage.getSourceClassName(), sourceClassPage
+						.getSourceClassFile(), sourceClassPage.getTestClassName(), sourceClassPage
+						.getTestClassFilePath(), sourceClassPage.getTestSuperClass());
+
+				if (ok) {
+					Path path = new Path(sourceClassPage.getTestClassFilePath());
+					IFile testFile = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+					IEditorInput editorInput = new FileEditorInput(testFile);
+					IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+					IWorkbenchPage page = window.getActivePage();
+					page.openEditor(editorInput, page.getActiveEditor().getSite().getId());
+				}
+
+				return ok;
+			} catch (InvalidObjectException e) {
+				Logger.logException(e);
+			} catch (CoreException e) {
+				Logger.logException(e);
+			} catch (InvalidClassException e) {
+				Logger.logException(e);
+			}
+		}
+
+		return false;
+	}
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.workbench = workbench;
+		this.selection = selection;
+	}
+
+	public void addPages() {
+		sourceClassPage = new PHPUnitTestCaseCreationWizardPage(selection);
+		addPage(sourceClassPage);
+	}
+
+	public boolean setSourceClassName(String className) {
+		return sourceClassPage.setSourceClassName(className);
+	}
+
+	public boolean setSourceClassName(String className, IDLTKSearchScope scope) {
+		return sourceClassPage.setSourceClassName(className, scope);
+	}
+
+	public boolean setSourceClassName(String className, IResource classFile) {
+		return sourceClassPage.setSourceClassName(className, classFile);
+	}
+}
