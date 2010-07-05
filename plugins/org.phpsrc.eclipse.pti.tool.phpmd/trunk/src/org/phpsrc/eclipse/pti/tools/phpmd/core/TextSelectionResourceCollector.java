9c072252bd0dba21a687fd8463a13dcac592cea9
spasic.dejan <spasic.dejan@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2010-07-05 22:33:01 +0000

    add license terms

--- plugins/org.phpsrc.eclipse.pti.tool.phpmd/trunk/src/org/phpsrc/eclipse/pti/tools/phpmd/core/TextSelectionResourceCollector.java
+++ plugins/org.phpsrc.eclipse.pti.tool.phpmd/trunk/src/org/phpsrc/eclipse/pti/tools/phpmd/core/TextSelectionResourceCollector.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2010 Dejan Spasic
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
 package org.phpsrc.eclipse.pti.tools.phpmd.core;
 
 import org.eclipse.ui.IEditorInput;
