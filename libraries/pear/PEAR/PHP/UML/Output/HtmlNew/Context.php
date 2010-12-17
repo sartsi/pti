01656f294b5bfd97131b30be1e8f6432d51ca6a3
skiera <skiera@0afbd939-c2ca-4ed0-a59d-02d098fe1c4f>
2010-12-17 23:00:05 +0000

    Automatic add new PEAR library

---
+++ libraries/pear/PEAR/PHP/UML/Output/HtmlNew/Context.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * PHP_UML
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_UML
+ * @author   Baptiste Autin <ohlesbeauxjours@yahoo.fr> 
+ * @license  http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ * @version  SVN: $Revision: 136 $
+ * @link     http://pear.php.net/package/PHP_UML
+ * @since    $Date: 2009-12-10 01:35:58 +0100 (jeu., 10 déc. 2009) $
+ */
+
+/**
+ * This class is a wrapper for data about the current package being processed
+ *
+ * @category   PHP
+ * @package    PHP_UML
+ * @subpackage Output
+ * @subpackage HtmlNew
+ * @author     Baptiste Autin <ohlesbeauxjours@yahoo.fr> 
+ * @license    http://www.gnu.org/licenses/lgpl.html LGPL License 3
+ */
+class PHP_UML_Output_HtmlNew_Context
+{
+    public $dir = '';
+    public $rpt = '';
+    public $classes;
+    public $interfaces;
+    public $datatypes;
+
+    public $allImplemented = array();
+    public $allImplementing = array();
+    public $allInherited = array();
+    public $allInheriting = array();
+}
+?>
